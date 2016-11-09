929a5c7bd86d397a58612e738ae5f47e6d922304
William Barbosa <william.sb@hotmail.com>
2016-11-09 20:20:34 -0200

    :fire: Move Nuspec to Mvx.Plugins

--- nuspec/ClipboardBootstrapAction.cs.pp
+++
@@ -1,7 +0,0 @@
-using MvvmCross.Platform.Plugins;
-
-namespace $rootnamespace$.Bootstrap
-{
-    public class ClipboardPluginBootstrap
-        : MvxPluginBootstrapAction<Clipboard.PluginLoader, Clipboard.iOS.Plugin> { }
-}
